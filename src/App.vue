95433d0394ed118a93f33bb819607c009eca5e00
Alex Law <pokemon999101@hotmail.co.uk>
2022-11-12 13:35:07 +0000

    ✨ Save paths to var

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,11 @@
     <canvas id="c"></canvas>
     <button @click="getJson">To Json</button>
     <button @click="loadJson">Load Json</button>
+    <button @click="loadFromPaths">Load from paths</button>
     <button @click="download">Download</button>
     <br />
     <textarea name="" id="" cols="30" rows="10" v-model="json"></textarea>
-    <p>{{ json.length }}</p>
+    <p>Json size: {{ json.length }}. Number of paths (different from json): {{ paths.length }}</p>
   </div>
 </template>
 
@@ -20,21 +21,47 @@ export default {
     return {
       canvas: null,
       json: '',
+      paths: [],
+      loadingFromPaths: false
     };
   },
   mounted() {
-    this.canvas = new fabric.Canvas('c', {
-      isDrawingMode: true,
-      height: 500,
-      width: 500,
-    });
-    let brush = new fabric.PencilBrush(this.canvas);
-    brush.width = 5;
-    brush.decimate = 10;
-    this.canvas.freeDrawingBrush = brush;
-    this.canvas.on('path:created', this.getJson);
+    this.initCanvas();
   },
   methods: {
+    initCanvas() {
+      this.canvas = new fabric.Canvas('c', {
+        isDrawingMode: true,
+        height: 500,
+        width: 500,
+      });
+      let brush = new fabric.PencilBrush(this.canvas);
+      brush.width = 5;
+      brush.decimate = 10;
+      this.canvas.freeDrawingBrush = brush;
+      this.canvas.on('path:created', (e) => {
+        if (this.loadingFromPaths) {
+          return;
+        }
+        this.getJson();
+        const json = JSON.stringify(e.path);
+        this.paths.push(json);
+      });
+    },
+    async loadFromPaths() {
+      this.loadingFromPaths = true;
+
+      this.canvas.clear();
+      let json = JSON.parse(this.json);
+      json.objects = [];
+      this.paths.forEach((path) => {
+        json.objects.push(JSON.parse(path));
+      })
+      this.json = JSON.stringify(json);
+      await this.loadJson();
+
+      this.loadingFromPaths = false;
+    },
     async getJson() {
       let json = this.canvas.toJSON();
       this.json = JSON.stringify(json);
